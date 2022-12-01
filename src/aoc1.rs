5d1bbdd6c79aec9bcdf91f987d5701f2d284704a
Fredrik Jansson <fj@avassa.io>
2022-12-01 09:22:07 +0100

    Day 1

--- src/aoc1.rs
+++ src/aoc1.rs
@@ -1,6 +1,5 @@
 pub fn run() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("day1.txt")?;
-    let input = parse(&input)?;
 
     println!("1:1 - {}", run_1(&input)?);
     println!("3:2 - {}", run_2(&input)?);
@@ -8,71 +7,66 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn parse(input: &str) -> anyhow::Result<Vec<usize>> {
-    let (_, res) = nom::multi::separated_list1(
-        nom::character::complete::newline,
-        crate::helper::uval::<usize>,
-    )(input)
-    .map_err(|e| e.to_owned())?;
-    Ok(res)
+type Input<'a> = &'a str;
+type PResult<'a> = nom::IResult<Input<'a>, Vec<Vec<u32>>, nom::error::VerboseError<Input<'a>>>;
+
+fn parse(input: Input) -> PResult {
+    let (i, res) = nom::multi::separated_list1(
+        nom::multi::many_m_n(2, 2, nom::character::complete::newline),
+        nom::multi::separated_list1(
+            nom::character::complete::newline,
+            nom::character::complete::u32,
+        ),
+    )(input)?;
+    Ok((i, res))
 }
 
-fn run_1(input: &[usize]) -> anyhow::Result<usize> {
-    let (_, cnt) = input.iter().fold((usize::MAX, 0), |(prev, cnt), cur| {
-        if &prev < cur {
-            (*cur, cnt + 1)
-        } else {
-            (*cur, cnt)
-        }
-    });
-    Ok(cnt)
+fn run_1(input_txt: &str) -> anyhow::Result<u32> {
+    let (_, input) = parse(&input_txt).map_err(|e| anyhow::anyhow!("{e}"))?;
+    let max = input.iter().map(|elf| elf.iter().sum()).max().unwrap();
+    Ok(max)
 }
 
-fn run_2(input: &[usize]) -> anyhow::Result<usize> {
-    let (_, cnt) =
-        input
-            .windows(3)
-            .map(|v| v.iter().sum())
-            .fold((usize::MAX, 0), |(prev, cnt), cur| {
-                if prev < cur {
-                    (cur, cnt + 1)
-                } else {
-                    (cur, cnt)
-                }
-            });
-    Ok(cnt)
+fn run_2(input_txt: &str) -> anyhow::Result<u32> {
+    let (_, input) = parse(&input_txt).map_err(|e| anyhow::anyhow!("{e}"))?;
+    let mut sums: Vec<u32> = input.iter().map(|elf| elf.iter().sum()).collect();
+    sums.sort_by(|a, b| b.cmp(a));
+
+    let top_3 = sums.iter().take(3).sum();
+    Ok(top_3)
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
+    const INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
 
     #[test]
     fn aoc1_parse() {
-        let result = super::parse(INPUT).unwrap();
-        assert_eq!(result.len(), 10);
-        assert_eq!(result[0], 199);
-        assert_eq!(result[9], 263);
+        let (_, result) = super::parse(INPUT).unwrap();
+        assert_eq!(result.len(), 5);
+        assert_eq!(result[0].len(), 3);
     }
 
     #[test]
     fn aoc1_run_1() {
-        let input = super::parse(INPUT).unwrap();
-        assert_eq!(super::run_1(&input).unwrap(), 7);
+        assert_eq!(super::run_1(INPUT).unwrap(), 24000);
     }
 
     #[test]
     fn aoc1_run_2() {
-        let input = super::parse(INPUT).unwrap();
-        assert_eq!(super::run_2(&input).unwrap(), 5);
+        assert_eq!(super::run_2(INPUT).unwrap(), 45000);
     }
 }
