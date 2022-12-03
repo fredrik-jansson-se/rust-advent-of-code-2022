846bd6983a5f1be6d3fade8ad2fbfd1e5eab41c4
Fredrik Jansson <fj@avassa.io>
2022-12-03 12:24:36 +0100

    Day 3

--- src/aoc3.rs
+++ src/aoc3.rs
@@ -8,110 +8,67 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn priority(c: char) -> usize {
+    const A_L: usize = 'a' as usize;
+    const A_U: usize = 'A' as usize;
+    if c.is_lowercase() {
+        c as usize - A_L + 1
+    } else {
+        c as usize - A_U + 27
+    }
+}
+
 fn run_1(input: &str) -> anyhow::Result<usize> {
-    let mut one_cnt = vec![0usize; 20];
-    let mut len = 0;
-
+    let mut sum = 0;
     for line in input.lines() {
-        one_cnt.truncate(line.len());
-        len += 1;
-        // let mut idx = 0;
-        for (idx, v) in line.chars().rev().enumerate() {
-            if v == '1' {
-                one_cnt[idx] += 1;
-            }
-            // idx += 1;
+        let l: Vec<char> = line.chars().collect();
+        let low = &l[..l.len() / 2];
+        let high = &l[(l.len() / 2)..];
+        // println!("{low:?} {high:?}");
+        if let Some(in_both) = low.iter().find(|c| high.contains(c)) {
+            sum += priority(*in_both);
         }
     }
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
-
-    for (idx, i) in one_cnt.iter().enumerate() {
-        if *i > (len / 2) {
-            gamma += 1 << idx;
-        } else {
-            epsilon += 1 << idx;
-        }
-    }
-    Ok(epsilon * gamma)
+    Ok(sum)
 }
 
-fn oxygen_rating(input: Vec<Vec<u32>>, idx: usize) -> Vec<u32> {
-    if input.len() == 1 {
-        return input[0].clone();
+fn run_2(input: &str) -> anyhow::Result<usize> {
+    use std::collections::HashSet;
+    let mut sum = 0;
+    let groups: Vec<&str> = input.lines().collect();
+
+    for group3 in groups.chunks(3) {
+        let mut first: HashSet<_> = group3[0].chars().collect();
+        first.retain(|f| group3[1].contains(|c| c == *f) && group3[2].contains(|c| c == *f));
+        sum += priority(*first.iter().next().unwrap());
     }
-
-    let num_ones = input.iter().filter(|line| line[idx] == 1).count();
-    let filter = (num_ones >= (input.len() - num_ones)) as u32;
-    let input = input
-        .into_iter()
-        .filter(|line| line[idx] == filter)
-        .collect();
-    oxygen_rating(input, idx + 1)
-}
-
-fn co2_rating(input: Vec<Vec<u32>>, idx: usize) -> Vec<u32> {
-    if input.len() == 1 {
-        return input[0].clone();
-    }
-
-    let num_ones = input.iter().filter(|line| line[idx] == 1).count();
-    let filter = (num_ones < (input.len() - num_ones)) as u32;
-    let input = input
-        .into_iter()
-        .filter(|line| line[idx] == filter)
-        .collect();
-    co2_rating(input, idx + 1)
-}
-
-fn run_2(input: &str) -> anyhow::Result<u32> {
-    let input: Vec<Vec<_>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u32)
-                .collect()
-        })
-        .collect();
-
-    let oxy = oxygen_rating(input.clone(), 0);
-    let mut oxy_val = 0;
-    for o in oxy.iter() {
-        oxy_val = (oxy_val << 1) + o;
-    }
-
-    let co2 = co2_rating(input, 0);
-    let mut co2_val = 0;
-    for c in co2.iter() {
-        co2_val = (co2_val << 1) + c;
-    }
-
-    Ok(co2_val * oxy_val)
+    Ok(sum)
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010";
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
+    fn aoc3_priority() {
+        assert_eq!(super::priority('a'), 1);
+        assert_eq!(super::priority('z'), 26);
+        assert_eq!(super::priority('A'), 27);
+        assert_eq!(super::priority('Z'), 52);
+    }
+    #[test]
     fn aoc3_run_1() {
-        assert_eq!(super::run_1(INPUT).unwrap(), 198);
+        assert_eq!(super::run_1(INPUT).unwrap(), 157);
     }
 
     #[test]
     fn aoc3_run_2() {
-        assert_eq!(super::run_2(INPUT).unwrap(), 230);
+        assert_eq!(super::run_2(INPUT).unwrap(), 70);
     }
 }
