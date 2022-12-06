7d66ccfe30ef28536006efc8613bc2859e0563cb
Fredrik Jansson <fj@avassa.io>
2022-12-06 08:49:17 +0100

    Day 6

--- src/aoc6.rs
+++ src/aoc6.rs
@@ -7,61 +7,59 @@ pub fn run() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn simulate(input: &str, days: usize) -> anyhow::Result<usize> {
-    let mut squids = parse(input)?;
-    for _ in 0..days {
-        let mut new_squids = vec![0; 9];
-        for (idx, count) in squids.iter().enumerate().filter(|(_, count)| **count > 0) {
-            if idx == 0 {
-                new_squids[6] += count;
-                new_squids[8] += count;
-            } else {
-                new_squids[idx - 1] += count;
-            }
-        }
-        squids = new_squids;
-    }
-    Ok(squids.iter().sum())
-}
-
 fn run_1(input: &str) -> anyhow::Result<usize> {
-    simulate(input, 80)
+    let input: Vec<_> = input.chars().collect();
+
+    for idx in 4..input.len() {
+        let sub = &input[(idx - 4)..idx];
+        if sub[0] != sub[1]
+            && sub[0] != sub[2]
+            && sub[0] != sub[3]
+            && sub[1] != sub[2]
+            && sub[1] != sub[3]
+            && sub[2] != sub[3]
+        {
+            return Ok(idx);
+        }
+    }
+    Ok(0)
 }
 
 fn run_2(input: &str) -> anyhow::Result<usize> {
-    simulate(input, 256)
-}
+    let input: Vec<_> = input.chars().collect();
 
-fn parse(i: &str) -> anyhow::Result<Vec<usize>> {
-    let (_, input): (&str, Vec<usize>) =
-        nom::multi::separated_list1(nom::bytes::complete::tag(","), crate::helper::uval)(i)
-            .map_err(|e| e.to_owned())?;
-
-    let mut res = vec![0; 9];
-    for i in input {
-        res[i] += 1;
+    for idx in 14..input.len() {
+        let sub: std::collections::HashSet<char> = input[(idx - 14)..idx].iter().copied().collect();
+        if sub.len() == 14 {
+            return Ok(idx);
+        }
     }
-    Ok(res)
+    Ok(0)
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "3,4,3,1,2";
-
-    #[test]
-    fn aoc6_parse() {
-        let squids = super::parse(INPUT).unwrap();
-        assert_eq!(squids[1], 1);
-        assert_eq!(squids[3], 2);
-    }
+    const INPUT_1: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    const INPUT_2: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    const INPUT_3: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+    const INPUT_4: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    const INPUT_5: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
 
     #[test]
     fn aoc6_run_1() {
-        assert_eq!(super::run_1(INPUT).unwrap(), 5934);
+        assert_eq!(super::run_1(INPUT_1).unwrap(), 7);
+        assert_eq!(super::run_1(INPUT_2).unwrap(), 5);
+        assert_eq!(super::run_1(INPUT_3).unwrap(), 6);
+        assert_eq!(super::run_1(INPUT_4).unwrap(), 10);
+        assert_eq!(super::run_1(INPUT_5).unwrap(), 11);
     }
 
     #[test]
     fn aoc6_run_2() {
-        assert_eq!(super::run_2(INPUT).unwrap(), 26984457539);
+        assert_eq!(super::run_2(INPUT_1).unwrap(), 19);
+        assert_eq!(super::run_2(INPUT_2).unwrap(), 23);
+        assert_eq!(super::run_2(INPUT_3).unwrap(), 23);
+        assert_eq!(super::run_2(INPUT_4).unwrap(), 29);
+        assert_eq!(super::run_2(INPUT_5).unwrap(), 26);
     }
 }
